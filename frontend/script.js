aa997b180de04097c806a87ae699efd1972c39d3
Alec <alechall.personal@gmail.com>
2026-09-12 22:19:50 +0100

    updated

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,25 @@
-const cards=document.querySelectorAll(".card");
-let current=0;
+const cards = document.querySelectorAll(".card");
+let current = 0;
 
-function update(i){
- current=(i+cards.length)%cards.length;
+function update(index) {
+    current = (index + cards.length) % cards.length;
 
- cards.forEach((card,n)=>{
-  let o=n-current;
+    cards.forEach((card, i) => {
+        let position = i - current;
 
-  if(o>cards.length/2)o-=cards.length;
-  if(o<-cards.length/2)o+=cards.length;
+        if (position > cards.length / 2) position -= cards.length;
+        if (position < -cards.length / 2) position += cards.length;
 
-  card.className="card";
+        card.className = "card";
 
-  if(o===0)card.classList.add("center");
-  else if(o===-1)card.classList.add("up-1");
-  else if(o===1)card.classList.add("down-1");
-  else if(o===-2)card.classList.add("up-2");
-  else if(o===2)card.classList.add("down-2");
-  else card.classList.add("hidden");
- });
+        if (position === 0) card.classList.add("center");
+        else if (position === -1) card.classList.add("up-1");
+        else if (position === 1) card.classList.add("down-1");
+        else if (position === -2) card.classList.add("up-2");
+        else if (position === 2) card.classList.add("down-2");
+        else card.classList.add("hidden");
+    });
 }
 
 update(0);
-setInterval(()=>update(current+1),4000);
+setInterval(() => update(current + 1), 4000);
