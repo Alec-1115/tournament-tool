828894d8570a6ebc7909cce1ad59365df7fef3dc
Alec <alechall.personal@gmail.com>
2026-09-13 19:36:40 +0100

    fixed Mobile Layout

--- frontend/script.js
+++ frontend/script.js
@@ -2,27 +2,76 @@ const cards = [...document.querySelectorAll(".card")];
 
 let current = 0;
 
+function isMobile() {
+    return window.innerWidth <= 700;
+}
+
 function updateCarousel() {
+
+    if (isMobile()) {
+
+        cards.forEach((card, index) => {
+
+            card.className = "card";
+
+            if (index === current) {
+                card.classList.add("mobile-center");
+            }
+
+            else if (
+                index === (current + 1) % cards.length
+            ) {
+                card.classList.add("mobile-next");
+            }
+
+            else if (
+                index === (current - 1 + cards.length) % cards.length
+            ) {
+                card.classList.add("mobile-prev");
+            }
+
+            else {
+                card.classList.add("mobile-hidden");
+            }
+
+        });
+
+        return;
+    }
+
     cards.forEach(card => {
         card.className = "card hidden";
     });
 
     cards.forEach((card, index) => {
-        const offset = (index - current + cards.length) % cards.length;
+
+        const offset =
+            (index - current + cards.length) % cards.length;
 
         if (offset === 0) {
             card.className = "card center";
-        } else if (offset === 1) {
+        }
+
+        else if (offset === 1) {
             card.className = "card down-1";
-        } else if (offset === 2) {
+        }
+
+        else if (offset === 2) {
             card.className = "card down-2";
-        } else if (offset === cards.length - 1) {
+        }
+
+        else if (offset === cards.length - 1) {
             card.className = "card up-1";
-        } else if (offset === cards.length - 2) {
+        }
+
+        else if (offset === cards.length - 2) {
             card.className = "card up-2";
-        } else {
+        }
+
+        else {
             card.className = "card hidden";
         }
+
     });
 }
 
@@ -34,3 +83,5 @@ function nextCard() {
 updateCarousel();
 
 setInterval(nextCard, 3500);
+
+window.addEventListener("resize", updateCarousel);
